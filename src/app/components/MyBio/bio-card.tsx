5207e8ee3585c4d1f05d78d98897768f71cb8c78
jcostadev23 <carlos_jesus_costa@hotmail.com>
2026-03-15 10:20:59 +0000

    bio-card

---
+++ src/app/components/MyBio/bio-card.tsx
@@ -0,0 +1,25 @@
+type Props = {
+  children: any;
+  backgroundImage?: string;
+};
+const BioCard: React.FC<Props> = ({ children, backgroundImage }) => {
+  return (
+    <div
+      className="glass-card neon-border rounded-3xl p-6 md:p-10 relative overflow-hidden group transition duration-500 hover:-translate-y-1 h-full flex flex-col"
+      style={{
+        ...(backgroundImage
+          ? {
+              backgroundImage: `url(${backgroundImage})`,
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+            }
+          : {}),
+      }}
+    >
+      <div className="absolute inset-0 bg-slate-950/60 group-hover:bg-slate-950/40 transition"></div>
+      <div className="relative rounded-2xl p-6 z-10">{children}</div>
+    </div>
+  );
+};
+
+export default BioCard;
