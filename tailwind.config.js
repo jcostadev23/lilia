e031119c40ed9bcbfabde9d0579bbfa5e30ad66c
jcostadev23 <carlos_jesus_costa@hotmail.com>
2026-03-15 18:15:42 +0000

    Adding space tokens and tw-migration steps

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,20 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    spacing: {
+      '0': '0px',
+      '2': 'var(--spacing-2)',
+      '3': 'var(--spacing-3)',
+      '4': 'var(--spacing-4)',
+      '6': 'var(--spacing-6)',
+      '8': 'var(--spacing-8)',
+      '10': 'var(--spacing-10)',
+      '12': 'var(--spacing-12)',
+      '16': 'var(--spacing-16)',
+      '20': 'var(--spacing-20)',
+      '24': 'var(--spacing-24)',
+      '32': 'var(--spacing-32)',
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
